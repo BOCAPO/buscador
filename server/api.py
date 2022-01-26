2481b6d31a9562fbb6c15e39a6072b56e76cf68f
BOCAPO <pauloinfo86@gmail.com>
2022-01-26 20:59:39 -0300

    Update api.py

--- server/api.py
+++ server/api.py
@@ -127,4 +127,4 @@ def getFromImpressos():
 if __name__ == "__main__":
     print( "oh hello" )
     #time.sleep(5)
-    app.run( host = "127.0.0.1", port = 5000 )
+    app.run( host = "0.0.0.0", port = 5000 )
